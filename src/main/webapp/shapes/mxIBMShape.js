c094441d4e63a0104f26026467fec3b3f9854ab3
jaywarfield-deveoper <jaywarfield.deveoper@gmail.com>
2022-03-14 19:50:48 -0400

    Update mxIBMShape.js

--- src/main/webapp/shapes/mxIBMShape.js
+++ src/main/webapp/shapes/mxIBMShape.js
@@ -347,6 +347,10 @@ mxIBMShapeBase.prototype.getCellStyles = function(shapeType, shapeLayout, hideIc
 	return styles;
 }
 
+// Get and set cell styles for events (layout changes, color changes TBD, etc).
+// data TBD = {shapeType, shapeLayout,
+//             lineColor, fillColor, fontColor, badgeColor,
+//             layoutChanged, colorChanged, hideIcon}`
 mxIBMShapeBase.prototype.setCellStyles = function(style, shapeType, shapeLayout, hideIcon)
 {
 	let styles = mxIBMShapeBase.prototype.getCellStyles(shapeType, shapeLayout, hideIcon);
